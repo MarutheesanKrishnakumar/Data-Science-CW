9a354b71896e500c8122b6f4e65bfd3f5c712e73
MarutheesanKrishnakumar <marutheesankrishnakumar7@gmail.com>
2024-05-11 00:41:23 +0530

    Updated Visualization

--- Streamlit.py
+++ Streamlit.py
@@ -16,74 +16,74 @@ st.markdown('<style>div.block-container{padding-top:1rem;}</style>',unsafe_allow
 
 
 # Filtering date
-col1, col2 = st.columns((2))
+chart1, chart2 = st.columns((2))
 Cleaned_data["Order Date"] = pd.to_datetime(Cleaned_data["Order Date"])
-
 # Getting the min and max date
 start_date = Cleaned_data["Order Date"].min()
 end_date = Cleaned_data["Order Date"].max()
 
-
-with col1:
+with chart1:
     date1 = st.date_input("Start Date", start_date)
 
-with col2:
+with chart2:
     date2 = st.date_input("End Date", end_date)
-
 Cleaned_data = Cleaned_data[(Cleaned_data["Order Date"] >= pd.to_datetime(date1)) & (Cleaned_data["Order Date"] <= pd.to_datetime(date2))].copy()
 
-# Creating two columns layout
-col1, col2 = st.columns(2)
-
+# Creating two chart layout
+chart1, chart2 = st.columns(2)
 # Summary Card for Total Profit
 total_profit = Cleaned_data['Profit'].sum()
-col1.metric("Total Profit", f"${total_profit:,.2f}")
+chart1.metric("Total Profit", f"${total_profit:,.2f}")
 
 # Summary Card for Total Profit
 total_sales = Cleaned_data['Sales'].sum()
-col2.metric("Total Sales", f"${total_sales:,.2f}")
+chart2.metric("Total Sales", f"${total_sales:,.2f}")
+
 
 SubCategory_df = Cleaned_data.groupby(by = ["Sub-Category"], as_index= False)["Sales"].sum()
-with col1:
+with chart1:
     st.subheader("Sales by Sub-Category")
     fig = px.bar(SubCategory_df, x = "Sub-Category", y = "Sales", text = ['${:,.2f}'.format(x) for x in SubCategory_df["Sales"]],
                  template = "seaborn")
     st.plotly_chart(fig,use_container_width=True, height = 200)
 
-with col2:
-    st.subheader("Sales by Region")
-    fig = px.pie(Cleaned_data, values = "Sales", names = "Region", hole = 0.5)
-    fig.update_traces(text = Cleaned_data["Region"], textposition = "outside")
+with chart2:
+    st.subheader("Sales by Segment")
+    fig = px.pie(Cleaned_data, values = "Sales", names = "Segment", hole = 0.5)
+    fig.update_traces(text = Cleaned_data["Segment"], textposition = "outside")
     st.plotly_chart(fig,use_container_width=True)
 
 
 Cleaned_data["Quarterly"] = Cleaned_data["Order Date"].dt.to_period("Q")
-
 # Filter to include only January, April, July, and October quarters
 filtered_df = Cleaned_data[Cleaned_data["Order Date"].dt.month.isin([1, 4, 7, 10])]
 
 st.subheader('Quarterly Sales')
-
 # Grouping Data by Quarters
 linechart = pd.DataFrame(filtered_df.groupby(filtered_df["Quarterly"].dt.to_timestamp(freq='Q'))["Sales"].sum()).reset_index()
-
 # Creating Line Chart
 fig2 = px.line(linechart, x="Quarterly", y="Sales", labels={"Sales": "Amount"}, height=500, width=1000, template="gridon")
-
 # Displaying the Chart
 st.plotly_chart(fig2, use_container_width=True)
 
+
 chart1, chart2 = st.columns((2))
-with chart1:    
-    # Box plot
-    fig_box = px.box(Cleaned_data, x='Category', y='Profit', title='Profit Obtained by Category', width=700, height=800)
-    st.plotly_chart(fig_box)
+with chart1:
+    # Calculate Quantity by ship mode
+    ShipMode_df = Cleaned_data.groupby(by="Ship Mode", as_index=False)["Quantity"].sum()
+    # Create column chart using Plotly Express
+    fig = px.bar(ShipMode_df, x="Ship Mode", y="Quantity",
+                 text=['${:,.2f}'.format(x) for x in ShipMode_df["Quantity"]])  # Format Quantity as Count
+    fig.update_traces(textposition='outside')  # Show text outside the bars
+    fig.update_layout(title='Products Shipping Mode', xaxis_title='Ship Mode', yaxis_title='Quantity')
+    st.plotly_chart(fig)
 
 with chart2:
-    # Group by Category and sum up Quantity
-    df_sum = Cleaned_data.groupby('Sub-Category')['Quantity'].sum().reset_index()
-    fig = px.bar(df_sum, x='Sub-Category', y='Quantity', title='Most Frequently Purchased Products',width=700, height=800)
-    st.plotly_chart(fig)
+    Market_Sales = Cleaned_data['Market'].value_counts().reset_index()
+    Market_Sales.columns = ['Market', 'Sales']
+    fig_pie = px.pie(Market_Sales, values='Sales', names='Market', title='Sales by Market')
+    st.plotly_chart(fig_pie, use_container_width=True)
+
 
 chart1, chart2 = st.columns((2))
 with chart1: 
@@ -91,17 +91,25 @@ with chart1:
     fig_heatmap = px.imshow(heatmap_data.pivot(index="Region", columns="Sub-Category", values="Sales"), 
                         labels=dict(x="Sub-Category", y="Region", color="Sales"),
                         title='Sales Heatmap by Sub-Category and Region',
-                        width=900, height=600)  # Specify width and height
+                        width=500, height=600)  # Specify width and height
     st.plotly_chart(fig_heatmap)
 
 with chart2:
-    fig = px.area(Cleaned_data, x='Region', y='Profit', title='Profit by Region',width=600, height=600)
+    # Scatter plot
+    fig_scatter = px.scatter(Cleaned_data, x='Sales', y='Profit', color='Category', title='Sales vs Profit', width=700, height=600)
+    st.plotly_chart(fig_scatter)
+
+
+chart1, chart2 = st.columns((2))
+with chart1:    
+    # Box plot
+    fig_box = px.box(Cleaned_data, x='Category', y='Profit', title='Profit Obtained by Category', width=700, height=1000)
+    st.plotly_chart(fig_box)
+
+with chart2:
+    fig = px.area(Cleaned_data, x='Region', y='Profit', title='Profit by Region',width=600, height=1000)
     st.plotly_chart(fig)
 
-
-# Scatter plot
-fig_scatter = px.scatter(Cleaned_data, x='Sales', y='Profit', color='Category', title='Sales vs Profit', width=1000, height=600)
-st.plotly_chart(fig_scatter)
 
 #MBA 
 from mlxtend.frequent_patterns import apriori
